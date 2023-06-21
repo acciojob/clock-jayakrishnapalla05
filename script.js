f5e85527e2d865b57ec651c73f617b0d1ea5aeac
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-21 14:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function update()
+	{
+		let element=document.getElementById("timer");
+		let date=new Date();
+		element.textContent=date.toLocaleString();
+		
+	}
+setInterval(update, 1000);
